92d24bd8d23a18b7df0121dcd84d5c9c90299a0e
kelly-zhu <79768096+kelly-zhu@users.noreply.github.com>
2025-08-13 16:47:01 -0700

    removed irrelevant files

--- src/query_rewriters/default.py
+++
@@ -1,7 +0,0 @@
-from ..interfaces import QueryRewriter, Query
-
-
-class DefaultQueryRewriter(QueryRewriter):
-    def process(self, query: Query) -> list[Query]:
-        # Example implementation: simply return the original query
-        return [query]
